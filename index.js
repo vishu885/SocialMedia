a4c38bdd6601e633f0fc19e931c2c6df611a17b3
vishva Shah <vishu885>
2024-02-29 16:11:35 -0500

    rendering first view using ejs

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express= require('express');
 const app=express();
 const port=8000;
 
+
 //use express router
 app.use('/' ,require('./routes/index.js'));
 app.set('view engine','ejs');
-app.set('view','./views');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express=require('express');
 const router=express.Router();
 const homeController =require('../controller/homeController.js');
 console.log("router loaded");
-//All request with / will come here first
+//All request from index.js  will come here first
 router.get('/',homeController.home);
-//then /user request frm here will go to user route
+//then /users request frm here will go to users route
 router.use('/users',require('./users'));
 
 module.exports=router;
